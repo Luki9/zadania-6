b453bfc57e8bff6d688e4c0e93324f6e451fa40d
lukasz <robson27-r@hotmail.com>
2019-01-25 11:41:07 +0100

    zadanie 1 zrobione

---
+++ src/Names.java
@@ -0,0 +1,15 @@
+public class Names {
+    public static void main(String[] args) {
+        String[]names = new String[3];
+        names[0] = "Beata";
+        names[1] = "Tomek";
+        names[2] = "Hubert";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+
+    }
+
+
+}
